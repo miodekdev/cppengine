09bd2825d05f3e05901725925b6594b2647e65c8
Miod3k <maksymilianmiodonski@gmail.com>
2024-03-30 20:22:36 +0100

    removed some left-over code

--- src/main.cpp
+++ src/main.cpp
@@ -5,9 +5,7 @@
 Graphics* graphics;
 
 void Start() {
-    Color d = Color::FromFloat(0, 0, 0.1, 1);
-    Color c = Color::FromByte(100);
-    graphics->Draw(10, 10, Color::FromFloat(1, 1, 0));
+
 }
 void MainLoop() {
 
--- src/physics.cpp
+++ src/physics.cpp
@@ -10,21 +10,17 @@ extern "C" {
 }
 
 float Vector::Distance(Vector vector1, Vector vector2) {
-    // Returns the distance between two points.
     return AsmVectorDistance(vector1, vector2);
 }
 
 float Vector::Magnitude() {
-    // Returns the magnitude (length) of a vector.
     return AsmVectorMagnitude(this);
 }
 
 Vector Vector::Normalized() {
-    // Normalizes a vector, making it a unit vector.
     return AsmVectorNormalized(this);
 }
 
 void Body::ApplySpeed() {
-    // Adds the speed of a body to its position.
     return AsmBodyApplySpeed(this);
 }

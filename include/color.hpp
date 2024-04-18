ab82edc90441dd7f45720f7f7d3fdc09b30045e3
Miod3k <maksymilianmiodonski@gmail.com>
2024-04-19 00:15:24 +0200

    Started changing assembly for intel intrinsics in color.cpp

--- include/color.hpp
+++ include/color.hpp
@@ -1,18 +1,19 @@
 #pragma once
 #include <d2d1_1.h>
 
-struct RGBA;
-
 struct RGBA {
-    BYTE red;
-    BYTE green;
-    BYTE blue;
-    BYTE alpha;
+    BYTE red{};
+    BYTE green{};
+    BYTE blue{};
+    BYTE alpha{};
 
-    static RGBA FromFloat(FLOAT* color);
-    static RGBA FromFloat(FLOAT red, FLOAT green, FLOAT blue, FLOAT alpha = 1.0);
-    static RGBA FromInt(UINT32 rgba);
-
+public:
+    explicit RGBA(UINT32 rgba);
+    RGBA() : red(0), green(0), blue(0), alpha(0) {};
+    RGBA(FLOAT red, FLOAT green, FLOAT blue, FLOAT alpha = 1.0);
     RGBA operator*(FLOAT multiplier);
     RGBA operator/(FLOAT multiplier);
+    operator UINT32();
+private:
+    explicit RGBA(__m128 xmm0);
 };
--- src/color.cpp
+++ src/color.cpp
@@ -1,25 +1,36 @@
 #include "color.hpp"
 
-extern "C" {
-    RGBA AssemblyRGBAFromFloatArray(FLOAT* color);
-    RGBA AssemblyMultiplyRGBAByFloat(RGBA color, FLOAT multiplier);
+static RGBA FloatsToRGBA(__m128 xmm0) {
+    __m128i xmm1 = _mm_cvtps_epi32(xmm0);
+    xmm1 = _mm_packs_epi32(xmm1, xmm1);
+    xmm1 = _mm_packus_epi16(xmm1, xmm1);
+    return RGBA(_mm_cvtsi128_si32(xmm1));
 }
 
-RGBA RGBA::FromFloat(FLOAT* color) {return AssemblyRGBAFromFloatArray(color);}
-
-RGBA RGBA::FromFloat(FLOAT red, FLOAT green, FLOAT blue, FLOAT alpha) {
-    FLOAT buffer[4] = {red, green, blue, alpha};
-    return RGBA::FromFloat(buffer);
+RGBA::RGBA(__m128 xmm0) {
+    __m128 xmm1 = {255, 255, 255, 255};
+    xmm0 = _mm_mul_ps(xmm0, xmm1);
+    *this = FloatsToRGBA(xmm0);
 }
 
-RGBA RGBA::FromInt(UINT32 rgba) {
-    return *(RGBA*)(void*)&rgba;
+RGBA::RGBA(FLOAT red, FLOAT green, FLOAT blue, FLOAT alpha) {
+    *this = RGBA(__m128{red, green, blue, alpha});
+}
+
+RGBA::RGBA(UINT32 rgba) {
+    *this = *(RGBA*)(void*)&rgba;
+}
+
+RGBA::operator UINT32() {
+    return *(UINT32*) (void*) this;
 }
 
 RGBA RGBA::operator*(FLOAT multiplier) {
-    return AssemblyMultiplyRGBAByFloat(*this, multiplier);
+    __m128i xmm0 = _mm_cvtsi32_si128(); //TODO
+    __m128 xmm1;
+    _mm_store_ss(&multiplier, xmm1);
 }
 
 RGBA RGBA::operator/(FLOAT multiplier) {
-    return AssemblyMultiplyRGBAByFloat(*this, 1/multiplier);
+    return this->operator*(1/multiplier);
 }
